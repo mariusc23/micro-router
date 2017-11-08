b8cdebd1744bcee53ccadcbb3fb2078d5056fb0c
Marius Craciunoiu <marius@craciunoiu.net>
2017-11-07 17:46:29 -0700

    Add support for baseUrl

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const url = require('url')
 const defineRoute = require('path-match')()
 
-const routerHandler = routes => next => (req, res) => {
+const routerHandler = (routes, options = {}) => {
+  const baseUrl = options.baseUrl || ''
   const paths = Object.keys(routes)
-  for (let i = 0; i < paths.length; i++) {
-    const path = paths[i]
-    const match = defineRoute(path)
-    const params = match(url.parse(req.url).pathname)
-    if (params) {
-      const route = routes[path]
-      req.params = params
-      return route(req, res)
+  return next => (req, res) => {
+    for (let i = 0; i < paths.length; i++) {
+      const path = paths[i]
+      const fullPath = `${baseUrl}${path}`
+      const match = defineRoute(fullPath)
+      const params = match(url.parse(req.url).pathname)
+      if (params) {
+        const route = routes[path]
+        req.params = params
+        return route(req, res)
+      }
     }
+    if (next) {
+      return next(req, res)
+    }
+    const err = new Error('Not Found')
+    err.statusCode = 404
+    throw err
   }
-  if (next) {
-    return next(req, res)
-  }
-  const err = new Error('Not Found')
-  err.statusCode = 404
-  throw err
 }
 
-const router = routes => (req, res) => {
+const router = (routes, options) => (req, res) => {
   // handler
   if (res) {
-    return routerHandler(routes)(null)(req, res)
+    return routerHandler(routes, options)(null)(req, res)
   }
   // middleware (req = next)
-  return routerHandler(routes)(req)
+  return routerHandler(routes, options)(req)
 }
 
 module.exports = {
